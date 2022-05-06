559b2589995256a3ee84c00cf93933ae1934ded5
MarcFly <mtorres.form@gmail.com>
2022-05-06 08:39:34 +0200

    Torso1

--- GamepadSDL/Game.cpp
+++ GamepadSDL/Game.cpp
@@ -13,6 +13,8 @@ bool Game::Init()
 	int legs = 2;
 	int arms = 4;
 
+	int torso = 1;
+
 	//Initialize SDL with all subsystems
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
